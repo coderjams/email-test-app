cf9b7f6bc451768a158017835cfa1083ca579e1c
jmcquarrie <jmcquarrie@wfscorp.com>
2024-09-06 12:07:12 -0400

    add backend and finish ux-ui look

---
+++ src/app/email-dialog/email-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './email-dialog.component.html',
+  styleUrl: './email-dialog.component.css'
+})
+export class EmailDialogComponent {
+
+
+}
